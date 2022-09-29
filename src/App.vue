c212469c9c4515d309ce5d0117546e28a8e63a4c
Simon Leger <regelnomis@hotmail.com>
2022-09-29 02:32:53 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app>
+    <div class="container">
+      <v-img style="width: 100%;" src="../public/img/header-background.jpeg"></v-img>
+      <div class="centered">
+        <h1>Groupe Touzel Leger inc <sub style="color: orange;">work in progress</sub></h1>
+      </div>
+    </div>
+    <ul class="navbar">
+      <li class="nav-item">Documents&nbsp;&nbsp;<v-icon>mdi-archive</v-icon></li>
+      <li class="nav-item">Zonage&nbsp;&nbsp;<v-icon>mdi-compass</v-icon></li>
+      <li class="nav-item">Services&nbsp;&nbsp;<v-icon>mdi-cards-spade</v-icon></li>
+      <li class="top-nav-icon"><v-img class="top-icon" src="../public/img/logo-rounded.jpg"></v-img></li>
+    </ul>
+    <HomePage/>
+  </v-app>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HomePage,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+
+<style>
+  h1, h2, h3, h4, h5, h6 {
+    font-size: 1.5vw;
+    color: rgb(23, 23, 136);
+  }
+  .navbar {
+    position: fixed;
+    top: 0;
+    height: auto;
+    background-color: white;
+    width: 100vw;
+    list-style-type: none;
+    z-index: 2;
+    padding: 1vw;
+    padding-right: 3vw;
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
+  }
+  li {
+    float: right;
+    margin: 0 10px 0 10px;
+    font-size: 1.3vw;
+  }
+  .top-nav-icon {
+    float: left;
+    cursor: pointer;
+  }
+  .nav-item {
+    padding: 0.5vw;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+  .nav-item:hover
+  {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+  .text {
+    font-size: 1.2vw;
+  }
+  .col{
+    text-align: center;
+  }
+  .container {
+    position: relative;
+    text-align: center;
+    color: white;
+  }
+  .centered {
+    position: absolute;
+    width: 30vw;
+    top: 50%;
+    left: 35vw;
+  }
+  .top-icon {
+    width: 3vw;
+  }
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,135 @@
+<template>
+<v-container>
+  <v-row class="section-divisions">
+    <v-spacer></v-spacer>
+    <v-col class="col" cols="3">
+      <v-img class="company-icon" src="../../public/img/logo-rounded.jpg"></v-img>
+      <h3 class="titre-divisions">Entretien de paysage</h3>
+      <p class="info-divisions text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur voluptates ad delectus veritatis neque obcaecati cumque voluptatem cupiditate ipsam quisquam aut velit, atque odio omnis molestiae accusamus? Harum, dolorem assumenda?</p>
+    </v-col>
+    <v-spacer></v-spacer>
+    <v-col class="col" cols="3">
+      <v-img class="company-icon" src="../../public/img/logo-rounded.jpg"></v-img>
+      <h3 class="titre-divisions">Boutique</h3>
+      <p class="info-divisions text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur voluptates ad delectus veritatis neque obcaecati cumque voluptatem cupiditate ipsam quisquam aut velit, atque odio omnis molestiae accusamus? Harum, dolorem assumenda?</p>
+    </v-col>
+    <v-spacer></v-spacer>
+    <v-col class="col" cols="3">
+      <v-img class="company-icon" src="../../public/img/logo-rounded.jpg"></v-img>
+      <h3 class="titre-divisions">Services Professionnels</h3>
+      <p class="info-divisions text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur voluptates ad delectus veritatis neque obcaecati cumque voluptatem cupiditate ipsam quisquam aut velit, atque odio omnis molestiae accusamus? Harum, dolorem assumenda?</p>
+    </v-col>
+    <v-spacer></v-spacer>
+    </v-row>
+    <v-row class="section-about">
+      <h2 class="titre-about">À propos de nous</h2>
+      <p class="info-about text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nam temporibus omnis sunt ullam quisquam praesentium alias doloremque voluptas dignissimos quia magni non, placeat architecto culpa delectus, fuga, necessitatibus vel? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum deserunt ipsa odio reprehenderit officiis, quisquam adipisci! At, magni ullam voluptatibus velit corporis aliquam ipsam consequatur error reiciendis repellendus, dolores soluta! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste qui nobis dolorum nemo officiis maxime culpa consequatur, similique ut enim obcaecati vitae harum magnam ab nesciunt ea, atque fugiat consequuntur!</p> 
+    </v-row>
+    <v-row class="section-moreInfo">
+      <v-col cols="4">
+        <div class="info-moreInfo1">
+          <h3 class="titre-moreInfo">Titre</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure molestias accusantium totam expedita, optio aliquam commodi quidem ex porro perferendis, sunt, sed repellat dicta error numquam accusamus asperiores officiis quia?</p>
+        </div>
+      </v-col>
+      <v-col cols="8">
+        <v-img class="image-moreInfo1" src="../../public/img/img1.jpeg"/>
+      </v-col>
+    </v-row>
+    <v-row class="section-moreInfo">
+      <v-col cols="8">
+        <v-img class="image-moreInfo2" src="../../public/img/img1.jpeg"/>
+      </v-col>
+      <v-col cols="4">
+        <div class="info-moreInfo2">
+          <h3 class="titre-moreInfo">Titre</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure molestias accusantium totam expedita, optio aliquam commodi quidem ex porro perferendis, sunt, sed repellat dicta error numquam accusamus asperiores officiis quia?</p>
+        </div>
+      </v-col>
+    </v-row>
+
+</v-container>
+</template>
+
+<style>
+  .v-container {
+    margin: 0;
+    padding: 0;
+  }
+  .titre-divisions {
+    margin: 10px;
+  }
+  .info-vlaue {
+    padding: 5px;
+    text-align: center;
+  }
+  .company-icon {
+    width: 3vw;
+    margin: auto;
+  }
+  .section-divisions {
+    width: 100vw;
+    padding: 30px;
+    background-color: white;
+  }
+  .section-about {
+    width: 100vw;
+    padding: 5vw 15vw 5vw 16vw;
+    background-color: rgb(37, 112, 37);
+    text-align: justify;
+    margin: 5vw 0 5vw 0;
+  }
+  .section-moreInfo {
+    width: 100vw;
+    padding: 1vw 15vw 1vw 16vw;
+    position: relative;
+  }
+  .titre-moreInfo {
+    color: white;
+    font-size: 2vw;
+  }
+  .image-moreInfo1 {
+    width: 700px;
+    box-shadow: 5px 5px 5px rgb(164, 164, 164);
+    border-radius: 10px;
+  }
+  .info-moreInfo1 {
+    color: white;
+    background-color: rgb(23, 23, 136);
+    padding: 2.5vw;
+    width: 35vw;
+    position: absolute;
+    right: 50vw;
+    z-index: 1;
+    margin-top: 3vw;
+    border-radius: 10px;
+    border: 2px solid white;
+    font-size: 1.5vw;
+  }
+  .image-moreInfo2 {
+    width: 700px;
+    box-shadow: 5px 5px 5px rgb(164, 164, 164);
+    border-radius: 10px;
+  }
+  .info-moreInfo2 {
+    color: white;
+    background-color: rgb(23, 23, 136);
+    padding: 2.5vw;
+    width: 35vw;
+    position: absolute;
+    left: 50vw;
+    z-index: 5;
+    margin-top: 3vw;
+    border-radius: 10px;
+    border: 2px solid white;
+    font-size: 1.5vw;
+  }
+
+  .titre-about {
+    color: white;
+    font-size: 3vw;
+  }
+  .info-about {
+    color: white;
+  }
+</style>
